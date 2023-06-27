036794c7f5669a4fb32b45c5601fdf489fb5c137
Ankit Mahajan <ankitmahajan76@gmail.com>
2023-06-26 23:01:04 -0600

    added option for finite difference function

--- ad_afqmc/pyscf_interface.py
+++ ad_afqmc/pyscf_interface.py
@@ -296,37 +296,37 @@ def write_dqmc(hcore, hcore_mod, chol, nelec, nmo, enuc, ms=0, filename='FCIDUMP
           fh5['mo_coeffs_up'] = mo_coeffs[0]
           fh5['mo_coeffs_dn'] = mo_coeffs[1]
 
-def finite_difference_properties(mol, observable, epsilon=1.e-5, norb_frozen=0):
+def finite_difference_properties(mol, observable, observable_constant=0., epsilon=1.e-5, norb_frozen=0):
   mf = scf.RHF(mol)
   h1e = mf.get_hcore() - epsilon * observable
   mf.get_hcore = lambda *args: h1e
   mf.verbose = 1
   mf.kernel()
-  emf_m = mf.e_tot
+  emf_m = mf.e_tot - epsilon * observable_constant
   mycc = cc.CCSD(mf)
   mycc.frozen = norb_frozen
   mycc.kernel()
-  emp2_m = mycc.e_hf + mycc.emp2
-  eccsd_m = mycc.e_tot
+  emp2_m = mycc.e_hf + mycc.emp2 - epsilon * observable_constant
+  eccsd_m = mycc.e_tot - epsilon * observable_constant
   et = mycc.ccsd_t()
-  eccsdpt_m = mycc.e_tot + et
+  eccsdpt_m = mycc.e_tot + et - epsilon * observable_constant
 
   mf = scf.RHF(mol)
   h1e = mf.get_hcore() + epsilon * observable
   mf.get_hcore = lambda *args: h1e
   mf.verbose = 1
   mf.kernel()
-  emf_p = mf.e_tot
+  emf_p = mf.e_tot + epsilon * observable_constant
   mycc = cc.CCSD(mf)
   mycc.frozen = norb_frozen
   mycc.kernel()
-  emp2_p = mycc.e_hf + mycc.emp2
-  eccsd_p = mycc.e_tot
+  emp2_p = mycc.e_hf + mycc.emp2 + epsilon * observable_constant
+  eccsd_p = mycc.e_tot + epsilon * observable_constant
   et = mycc.ccsd_t()
-  eccsdpt_p = mycc.e_tot + et
+  eccsdpt_p = mycc.e_tot + et + epsilon * observable_constant
 
-  print(f'emf_m: {emf_m}, emf_p: {emf_p}, dip_mf: {(emf_p - emf_m) / 2 / epsilon}')
-  print(f'emp2_m: {emp2_m}, emp2_p: {emp2_p}, dip_mp2: {(emp2_p - emp2_m) / 2 / epsilon}')
-  print(f'eccsd_m: {eccsd_m}, eccsd_p: {eccsd_p}, dip_ccsd: {(eccsd_p - eccsd_m) / 2 / epsilon}')
-  print(f'eccsdpt_m: {eccsdpt_m}, eccsd_p: {eccsdpt_p}, dip_ccsdpt: {(eccsdpt_p - eccsdpt_m) / 2 / epsilon}')
+  print(f'emf_m: {emf_m}, emf_p: {emf_p}, obs_mf: {(emf_p - emf_m) / 2 / epsilon}')
+  print(f'emp2_m: {emp2_m}, emp2_p: {emp2_p}, obs_mp2: {(emp2_p - emp2_m) / 2 / epsilon}')
+  print(f'eccsd_m: {eccsd_m}, eccsd_p: {eccsd_p}, obs_ccsd: {(eccsd_p - eccsd_m) / 2 / epsilon}')
+  print(f'eccsdpt_m: {eccsdpt_m}, eccsd_p: {eccsdpt_p}, obs_ccsdpt: {(eccsdpt_p - eccsdpt_m) / 2 / epsilon}')
 
