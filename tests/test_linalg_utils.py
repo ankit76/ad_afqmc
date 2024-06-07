37564f3e24d65c7125c562827b50b703c1aef2e5
Ankit Mahajan <ankitmahajan76@gmail.com>
2024-06-07 00:18:51 -0400

    fixed test

--- tests/test_linalg_utils.py
+++ tests/test_linalg_utils.py
@@ -15,14 +15,14 @@ np.random.seed(seed)
 
 def test_modified_cholesky():
     norb = 5
-    size = norb * (norb + 1) // 2
-    mat = np.random.rand(size, size)
+    size = norb * norb
+    mat = np.random.randn(size, size)
     mat = mat @ mat.T
     mat = jnp.array(mat)
-    chol = linalg_utils.modified_cholesky(mat, norb)
+    chol = linalg_utils.modified_cholesky(mat, norb, size)
     mat_chol = 0.0 * mat
     for i in range(chol.shape[0]):
-        chol_vec = chol[i].reshape(norb, norb)[np.tril_indices(norb)]
+        chol_vec = chol[i]
         mat_chol += np.einsum("i,j->ij", chol_vec, chol_vec)
     assert np.allclose(mat, mat_chol)
 
