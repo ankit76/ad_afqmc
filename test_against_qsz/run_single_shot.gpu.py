7c9e1dee0b4d8c75a9ec8944f0d18c6795881d41
Shu Fay Ung <shufay.ung@gmail.com>
2025-05-30 14:01:54 -0500

    Added the wrong folder before!

--- test_against_qsz/run_single_shot.gpu.py
+++ test_against_qsz/run_single_shot.gpu.py
@@ -2,13 +2,13 @@ import os
 import sys
 import h5py
 import argparse
+from glob import glob
 import numpy as np
 import scipy as sp
 
 import jax
 jax.config.update("jax_debug_nans", True)
 
-from jax import vmap, jit, numpy as jnp, random, lax, jvp, scipy as jsp
 from pyscf import fci, gto, scf, mp, ao2mo
 from io_tools import check_dir
 
@@ -18,6 +18,8 @@ if module_path not in sys.path:
 
 from ad_afqmc import config
 config.afqmc_config['use_gpu'] = True
+
+from scc import *
 
 from ad_afqmc import (
     driver,
@@ -39,7 +41,8 @@ np.set_printoptions(precision=5, suppress=True)
 rank = mpi_jax.rank
 comm = mpi_jax.comm
 
-def save(fname, mf, dm):
+def save(fname, mf, dm=None):
+    if dm is None: dm = mf.make_rdm1()
     print(f'\n# Saving UHF trial to {fname}')
 
     with h5py.File(f'{fname}.h5', 'w') as h5:
@@ -55,7 +58,6 @@ parser = argparse.ArgumentParser()
 
 # Required.
 parser.add_argument('--U', type=float, required=True)
-parser.add_argument('--Ueff', type=float, required=True)
 parser.add_argument('--nup', type=int, required=True)
 parser.add_argument('--ndown', type=int, required=True)
 parser.add_argument('--nx', type=int, required=True)
@@ -69,14 +71,17 @@ parser.add_argument('--n_eql', type=int, required=False, nargs='?', default=300)
 parser.add_argument('--n_blocks', type=int, required=False, nargs='?', default=400)
 parser.add_argument('--run_cpmc', type=int, required=False, nargs='?', default=0)
 parser.add_argument('--set_e_estimate', type=int, required=False, nargs='?', default=0)
+parser.add_argument('--init_trial', type=str, required=False, nargs='?', default='fe_single_occ')
+parser.add_argument('--Ueff', type=float, required=False, nargs='?')
 parser.add_argument('--pin_type', type=str, required=False, nargs='?', default='fm')
 parser.add_argument('--v', type=float, required=False, nargs='?', default=0.25)
+parser.add_argument('--proj_trs', type=int, required=False, nargs='?', default=0)
+parser.add_argument('--approx_dm_pure', type=int, required=False, nargs='?', default=0)
 parser.add_argument('--verbose', type=int, required=False, nargs='?', default=0)
 
 args = parser.parse_args()
 
 U = args.U
-Ueff = args.Ueff
 nup = args.nup
 ndown = args.ndown
 nx = args.nx
@@ -89,20 +94,49 @@ n_eql = args.n_eql
 n_blocks = args.n_blocks
 run_cpmc = args.run_cpmc
 set_e_estimate = args.set_e_estimate
+init_trial = args.init_trial
+Ueff = args.Ueff
 pin_type = args.pin_type
 v = args.v
 verbose = args.verbose if rank == 0 else 0
 
-if verbose: 
-    print(f'\n# Ueff: {Ueff}')
-    print(f'# Boundary condition: {bc}')
+proj_trs = args.proj_trs
+trs_tag = ''
+if proj_trs: trs_tag = 'trs'
+
+approx_dm_pure = args.approx_dm_pure
+dm_tag = 'dm_mixed'
+if approx_dm_pure: dm_tag = 'approx_dm_pure'
+
+if verbose: print(f'\n# Boundary condition: {bc}')
 
 # For saving.
-tmpdir = f'/projects/bcdd/shufay/hubbard_tri/bcs/test_against_qsz/uhf/{nx}x{ny}/{bc}/dt={dt}/U={U}/Ueff={Ueff}/pin={pin_type}/'
+tmpdir = f'/projects/bcdd/shufay/hubbard_tri/self_consistent_constraint/test_against_qsz/{trs_tag}/{dm_tag}/{nx}x{ny}/{bc}/U={U}/pin={pin_type}/{init_trial}_trial/'
+if rank == 0: check_dir(tmpdir)
 jobid = ''
 try: jobid = '.' + os.environ["SLURM_JOB_ID"]
 except: pass
+filename = f'scc{jobid}'
 
+# -----------------------------------------------------------------------------
+n_elec = (nup, ndown)
+
+options = {
+    "dt": dt,
+    "n_eql": n_eql,
+    "n_ene_blocks_eql": 1,
+    "n_sr_blocks_eql": 1,
+    "n_ene_blocks": 1,
+    "n_sr_blocks": 5,
+    "n_blocks": n_blocks,
+    "n_prop_steps": 50,
+    "n_walkers": nwalkers,
+    "seed": 98,
+    "walker_type": "uhf",
+    "trial": "uhf",
+    "save_walkers": False,
+    "ad_mode": "mixed",
+}
 # -----------------------------------------------------------------------------
 # Settings.
 open_x = False
@@ -112,23 +146,27 @@ n_sites = lattice.n_sites
 n_elec = (nup, ndown)
 nocc = sum(n_elec)
 filling = nocc / (2*n_sites)
-density = nocc / n_sites
+density = sum(n_elec) / n_sites
 
 if verbose: 
     print(f'\n# Filling factor = {filling}')
     print(f'# Density = {density}')
+    print(f'\n# Pinning field = {pin_type}')
+    print(f'# Pinning field strength = {v}')
+    print(f'\n# Ueff = {Ueff}')
 
 # -----------------------------------------------------------------------------
 # Integrals.
 integrals = {}
+integrals["u"] = U
 integrals["h0"] = 0.0
-
+    
 # Nearest-neighbour hopping.
 h1 = -1.0 * lattice.create_adjacency_matrix()
 
 # Pinning field.
-v1a = np.zeros(n_sites)
-v1b = np.zeros(n_sites)
+v1a = numpy.zeros(n_sites)
+v1b = numpy.zeros(n_sites)
 
 # Assuming the y-direction is the short one with PBC.
 for iy in range(lattice.l_y):
@@ -146,7 +184,7 @@ for iy in range(lattice.l_y):
         v1a[site_num_right] = -val
         v1b[site_num_right] = val
 
-integrals["h1"] = np.array([h1 + np.diag(v1a), h1 + np.diag(v1b)])
+integrals["h1"] = numpy.array([h1 + numpy.diag(v1a), h1 + numpy.diag(v1b)])
 
 h2 = np.zeros((n_sites, n_sites, n_sites, n_sites))
 for i in range(n_sites): h2[i, i, i, i] = U
@@ -167,25 +205,32 @@ mol.verbose = verbose
 mol.build()
 
 # UHF trial.
-seed = 262
-np.random.seed(seed)
-if verbose: print(f'np.random.seed = {seed}')
+#npz_fname = glob(f'{tmpdir}rdm1_uhf.npz')[0]
+#dm = npz['rdm1_uhf']
+
+npz_fname = sorted(glob(f'{tmpdir}rdm1_afqmc_*.npz'))[-1]
+npz = np.load(npz_fname)
+dm = npz['rdm1_avg']
+if verbose: print(f'\n# Loading dm from {npz_fname}...')
+
+if approx_dm_pure:
+    npz_fname = glob(f'{tmpdir}rdm1_uhf.npz')[0]
+    npz = np.load(npz_fname)
+    
+    if verbose: 
+        print(f'\n# Approximating the pure rdm1...')
+        print(f'# Loading UHF rdm1 from {npz_fname}...')
+    
+    dm_hf = npz['rdm1_uhf']
+    dm = 2*dm - dm_hf
 
 umf = scf.UHF(mol)
 umf.get_hcore = lambda *args: integrals["h1"]
 umf.get_ovlp = lambda *args: np.eye(n_sites)
 umf._eri = h2_hf
 
-dm_init = 0.0 * umf.init_guess_by_1e()
-for ix in range(lattice.l_x):
-    for iy in range(lattice.l_y):
-        site_num = iy * lattice.l_x + ix
-        if (ix + iy) % 2 == 0: dm_init[0, site_num, site_num] = 1.0
-        else: dm_init[1, site_num, site_num] = 1.0
-
 umf.max_cycle = 1000
-umf.level_shift = 0.5
-umf.kernel(dm_init)
+umf.kernel(dm)
 mo1 = umf.stability()[0]
 umf = umf.newton().run(mo1, umf.mo_occ)
 mo1 = umf.stability()[0]
@@ -194,6 +239,8 @@ mo1 = umf.stability()[0]
 umf = umf.newton().run(mo1, umf.mo_occ)
 mo1 = umf.stability()
 
+save(f'{tmpdir}mf_conv', umf)
+
 # -----------------------------------------------------------------------------
 # Prep AFQMC.
 if rank == 0:
@@ -201,48 +248,53 @@ if rank == 0:
             umf, basis_coeff=np.eye(n_sites), integrals=integrals, tmpdir=tmpdir)
 
 comm.Barrier()
-options = {
-    "dt": dt,
-    "n_eql": n_eql,
-    "n_ene_blocks_eql": 1,
-    "n_sr_blocks_eql": 5,
-    "n_ene_blocks": 1,
-    "n_sr_blocks": 5,
-    "n_blocks": n_blocks,
-    "n_prop_steps": 50,
-    "n_walkers": nwalkers,
-    "seed": 98,
-    "walker_type": "uhf",
-    "trial": "uhf",
-    "save_walkers": False,
-    "ad_mode": "mixed",
-}
-
 ham_data, ham, prop, trial, wave_data, sampler, observable, options, MPI = (
     mpi_jax._prep_afqmc(options, tmpdir=tmpdir)
 )
 
-trial = wavefunctions.uhf_cpmc(n_sites, n_elec)
+if run_cpmc:
+    if verbose: print(f'\n# Using CPMC propagator...')
+    prop = propagation.propagator_cpmc(
+        dt=options["dt"],
+        n_walkers=options["n_walkers"],
+    )
+
+    if proj_trs:
+        prop = propagation.propagator_cpmc_slow(
+            dt=options["dt"],
+            n_walkers=options["n_walkers"],
+        )
+    
 wave_data["mo_coeff"] = jnp.array([umf.mo_coeff[0][:, :n_elec[0]], 
                                    umf.mo_coeff[1][:, :n_elec[1]]])
-wave_data["rdm1"] = jnp.array(umf.make_rdm1())
 ham_data["u"] = U
+
+if proj_trs: 
+    if verbose: print(f'\n# Applying TRS projection for UHF trials...')
+    trial, wave_data = project_trs_trial(umf, wave_data)
+
+else: trial = wavefunctions.uhf_cpmc(n_sites, n_elec)
 
 if set_e_estimate:
     # GHF energy for `e_estimate`.
     dm_init = 0.0 * umf.init_guess_by_1e()
 
-    for ix in range(lattice.l_x):
-        for iy in range(lattice.l_y):
-            site_num = iy * lattice.l_x + ix
-            if (ix + iy) % 2 == 0: dm_init[0, site_num, site_num] = 1.0
-            else: dm_init[1, site_num, site_num] = 1.0
+    for i in range(lattice.l_x):
+        for j in range(lattice.l_y):
+            site_num = j * lattice.l_y + i
+
+            if (i + j) % 2 == 0:
+                dm_init[0, site_num, site_num] = 1.0
+
+            else:
+                dm_init[1, site_num, site_num] = 1.0
 
     gmf = scf.GHF(mol)
     gmf.get_hcore = lambda *args: sp.linalg.block_diag(*integrals["h1"])
     gmf.get_ovlp = lambda *args: np.eye(2 * n_sites)
     gmf._eri = ao2mo.restore(8, integrals["h2"], n_sites)
     dm_init = sp.linalg.block_diag(dm_init[0], dm_init[1])
+
     gmf.kernel(dm_init)
     mo1 = gmf.stability(external=True)
     gmf = gmf.newton().run(mo1, gmf.mo_occ)
@@ -258,14 +310,6 @@ if set_e_estimate:
 
 # -----------------------------------------------------------------------------
 # Run AFQMC.
-if run_cpmc:
-    if verbose: print(f'\n# Using CPMC propagator...')
-    #prop = propagation.propagator_cpmc_slow(
-    prop = propagation.propagator_cpmc(
-        dt=options["dt"],
-        n_walkers=options["n_walkers"],
-    )
-
 e_afqmc, err_afqmc = driver.afqmc(
     ham_data, ham, prop, trial, wave_data, sampler, observable, options, MPI,
     tmpdir=tmpdir
--- test_against_qsz/srun_scc.gpu.sh
+++ test_against_qsz/srun_scc.gpu.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
 #SBATCH --partition=gpuA100x4
+##SBATCH --partition=gpuA100x4-preempt
 #SBATCH --account=bcdd-delta-gpu
 #SBATCH --gpus-per-node=1
 #SBATCH --gpu-bind=closest   # select a cpu close to gpu on pci bus topology
 #SBATCH --ntasks-per-node=1  # could be 1 for py-torch
 #SBATCH --cpus-per-task=4   # spread out to use 1 core per numa, set to 64 if tasks is 1
 #SBATCH --constraint="scratch"
-#SBATCH --time=06:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem=10g
 #SBATCH --export=ALL
 #SBATCH --no-requeue
@@ -24,7 +25,6 @@ source $HOME/.bashrc
 conda deactivate                                                                   
 conda deactivate                                                                   
 conda activate ad_afqmc_gpu
-#conda activate ad_afqmc3
                                                                                    
 export OMP_NUM_THREADS=1
 
@@ -40,41 +40,47 @@ echo
 
 # Inputs.
 U=${1}
-Ueff=${2}
-nup=${3}
-ndown=${4}
-nx=${5}
-ny=${6}
-nwalkers=${7}
-bc=${8}
-run_cpmc=${9}
-set_e_estimate=${10}
-pin_type=${11}
-v=${12}
-dt=${13}
-n_eql=${14}
-n_blocks=${15}
-verbose=${16}
+nup=${2}
+ndown=${3}
+nx=${4}
+ny=${5}
+nwalkers=${6}
+bc=${7}
+run_cpmc=${8}
+set_e_estimate=${9}
+dt=${10}
+n_eql=${11}
+n_blocks=${12}
+init_trial=${13}
+Ueff=${14}
+pin_type=${15}
+v=${16}
+proj_trs=${17}
+approx_dm_pure=${18}
+tol_delta_min=${19}
+verbose=${20}
 
-export JAX_TRACEBACK_FILTERING=off
-
-python -u run_ueff_uhf_trial.gpu.py \
-                                    --U ${U} \
-                                    --Ueff ${Ueff} \
-                                    --nup ${nup} \
-                                    --ndown ${ndown} \
-                                    --nx ${nx} \
-                                    --ny ${ny} \
-                                    --nwalkers ${nwalkers} \
-                                    --dt ${dt} \
-                                    --n_eql ${n_eql} \
-                                    --n_blocks ${n_blocks} \
-                                    --bc ${bc} \
-                                    --run_cpmc ${run_cpmc} \
-                                    --set_e_estimate ${set_e_estimate} \
-                                    --pin_type ${pin_type} \
-                                    --v ${v} \
-                                    --verbose ${verbose}
+python -u run_scc.gpu.py \
+            --U ${U} \
+            --nup ${nup} \
+            --ndown ${ndown} \
+            --nx ${nx} \
+            --ny ${ny} \
+            --nwalkers ${nwalkers} \
+            --dt ${dt} \
+            --n_eql ${n_eql} \
+            --n_blocks ${n_blocks} \
+            --bc ${bc} \
+            --run_cpmc ${run_cpmc} \
+            --set_e_estimate ${set_e_estimate} \
+            --init_trial ${init_trial} \
+            --Ueff ${Ueff} \
+            --pin_type ${pin_type} \
+            --v ${v} \
+            --proj_trs ${proj_trs} \
+            --approx_dm_pure ${approx_dm_pure} \
+            --tol_delta_min ${tol_delta_min} \
+            --verbose ${verbose}
 
 # Mark the time job finishes
 date
