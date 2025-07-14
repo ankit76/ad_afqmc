45e43923b4596d4e4ca5fd8fa565061caf177bea
Ankit Mahajan <ankitmahajan76@gmail.com>
2025-07-13 23:55:06 -0400

    removed duplicate code

--- ad_afqmc/walkers.py
+++ ad_afqmc/walkers.py
@@ -118,35 +118,10 @@ tree_util.register_pytree_node(
 )
 
 
-@dataclass
-class GHFWalkers(walker_batch):
-    """Wrapper for GHF walkers"""
+class GHFWalkers(RHFWalkers):
+    """Wrapper for GHF walkers, implementation same as RHF."""
 
-    data: jax.Array
-
-    @property
-    def n_walkers(self) -> int:
-        return self.data.shape[0]
-
-    def apply_chunked(
-        self, apply_fn: Callable, n_chunks: int, *args, **kwargs
-    ) -> jax.Array:
-        """Apply function to GHF walkers in sequential chunks."""
-        chunk_size = self.n_walkers // n_chunks
-
-        def scanned_fun(carry, walker_chunk):
-            result_chunk = vmap(apply_fn, in_axes=(0, *[None] * len(args)))(
-                walker_chunk, *args, **kwargs
-            )
-            return carry, result_chunk
-
-        _, results = lax.scan(
-            scanned_fun,
-            None,
-            self.data.reshape(n_chunks, chunk_size, *self.data.shape[1:]),
-        )
-
-        return results.reshape(self.n_walkers, *results.shape[2:])
+    pass
 
 
 tree_util.register_pytree_node(
