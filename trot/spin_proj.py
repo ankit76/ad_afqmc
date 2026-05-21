762e4a9eddd820e6be37ca86383b206b5b8e3ab1
Ydrnan <yann.damour@hotmail.fr>
2026-05-21 14:44:45 -0700

    spin proj minor refactor

--- trot/spin_proj.py
+++ trot/spin_proj.py
@@ -3,45 +3,31 @@ from typing import Any, Callable
 import jax
 import jax.numpy as jnp
 
+from .trial.multi_ghf import _rotate_spin_trial, _make_beta_grid
 
-def apply_unitary(wi_a: jax.Array, wi_b: jax.Array, U: jax.Array) -> jax.Array:
+
+def _rotate_spin_uw(wa: jax.Array, wb: jax.Array, beta: jax.Array) -> jax.Array:
     """
-    Builds a GHF walker from a rotated UHF walker.
+    Rotate the spin of a UHF walker to build a GHF walker.
 
     Input:
-    wi_a: alpha part of the UHF walker
-    wi_b: beta  part of the UHF walker
-    U   : Unitary matrix
+    wa: alpha part of the UHF walker
+    wb: beta  part of the UHF walker
+    beta: Rotation (e^-beta S_y)
 
     Output:
-    wi_g: GHF walker
+    wg: GHF walker
     """
-    A, B = wi_a * U[0, 0], wi_b * U[0, 1]
-    C, D = wi_a * U[1, 0], wi_b * U[1, 1]
+    norb = wa.shape[0]
+    wg = jnp.block(
+        [
+            [wa, jnp.zeros_like(wb)],
+            [jnp.zeros_like(wa), wb],
+        ]
+    )
+    wg = _rotate_spin_trial(wg, norb=norb, beta=beta, alpha=jnp.array(0.0))
 
-    wi_g = jnp.block([[A, B], [C, D]])
-    return wi_g
-
-
-def build_unitary(betas: jax.Array) -> jax.Array:
-    """
-    Builds 2 by 2 unitary matrices.
-
-    Input:
-    betas: rotation angles
-
-    Output:
-    U: Unitary matrices corresponding to the different rotation angles
-    """
-    U = jax.vmap(
-        lambda beta: jnp.array(
-            [
-                [jnp.cos(beta / 2), jnp.sin(beta / 2)],
-                [-jnp.sin(beta / 2), jnp.cos(beta / 2)],
-            ]
-        )
-    )(betas)
-    return U
+    return wg
 
 
 def quadrature_s2(
@@ -59,18 +45,13 @@ def quadrature_s2(
     betas  : grid points
     w_betas: weights associated with each grid point
     """
-    from numpy.polynomial.legendre import leggauss
-
-    x, w = leggauss(ngrid)
-    betas = jnp.arccos(x)
-
     S = 0.5 * target_spin
     na, nb = nelec
     assert na >= nb
     Sz = 0.5 * (na - nb)
 
+    betas, w = _make_beta_grid(ngrid)
     wigner = lambda beta: wigner_small_d(S, Sz, Sz, beta)
-
     w_betas = jax.vmap(wigner)(betas) * w * (2.0 * S + 1.0) * 0.5
 
     return betas, w_betas
@@ -91,18 +72,18 @@ def make_overlap_u_s2(
     overlap_u_s2: function to compute the overlap using S^2 spin projection
     """
 
-    U = build_unitary(betas)
-
-    def overlap_u_s2(walker: Any, trial_data: Any):
+    def overlap_u_s2(
+        walker: tuple[jax.Array, jax.Array],
+        trial_data: Any,
+    ) -> jax.Array:
         wa, wb = walker
 
-        def _wrapper(U_i):
-            wg = apply_unitary(wa, wb, U_i)
+        def _wrapper(beta):
+            wg = _rotate_spin_uw(wa, wb, beta)
             o_i = overlap_g(wg, trial_data)
             return o_i
 
-        # o = jax.vmap(_wrapper, (0,))(U)
-        o = jax.lax.map(_wrapper, U)
+        o = jax.lax.map(_wrapper, betas)
 
         return jnp.sum(o * w_betas)
 
@@ -127,19 +108,18 @@ def make_energy_kernel_uw_rh_s2(
     energy_kernel_uw_rh_s2: function to compute the local energy using S^2 spin projection
     """
 
-    U = build_unitary(betas)
-
-    def energy_kernel_uw_rh_s2(walker, ham_data, meas_ctx, trial_data):
+    def energy_kernel_uw_rh_s2(
+        walker: tuple[jax.Array, jax.Array], ham_data: Any, meas_ctx: Any, trial_data: Any
+    ) -> jax.Array:
         wa, wb = walker
 
-        def _wrapper(U_i):
-            wg = apply_unitary(wa, wb, U_i)
+        def _wrapper(beta_i):
+            wg = _rotate_spin_uw(wa, wb, beta_i)
             e_i = energy_kernel_gw_rh(wg, ham_data, meas_ctx, trial_data)
             o_i = overlap_g(wg, trial_data)
             return o_i, e_i
 
-        # o, e = jax.vmap(_wrapper, (0,))(U)
-        o, e = jax.lax.map(_wrapper, U)
+        o, e = jax.lax.map(_wrapper, betas)
 
         return jnp.sum(e * o * w_betas) / jnp.sum(o * w_betas)
 
